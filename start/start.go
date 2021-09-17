cfd0743d8de3348674d7310335adefaee05afe4d
shnaki <tehnca-nome@hotmail.co.jp>
2021-09-17 23:20:02 +0900

    feat: Complete example

--- start/start.go
+++ start/start.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/shnaki/ent-example/ent"
 	"github.com/shnaki/ent-example/ent/car"
+	"github.com/shnaki/ent-example/ent/group"
 	"github.com/shnaki/ent-example/ent/user"
 )
 
@@ -42,6 +43,18 @@ func main() {
 		log.Fatal(err)
 	}
 	if err := QueryCarUsers(ctx, a8m); err != nil {
+		log.Fatal(err)
+	}
+	if err := CreateGraph(ctx, client); err != nil {
+		log.Fatal(err)
+	}
+	if err := QueryGithub(ctx, client); err != nil {
+		log.Fatal(err)
+	}
+	if err := QueryArielCars(ctx, client); err != nil {
+		log.Fatal(err)
+	}
+	if err := QueryGroupWithUsers(ctx, client); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -132,7 +145,8 @@ func QueryCarUsers(ctx context.Context, a8m *ent.User) error {
 	if err != nil {
 		return fmt.Errorf("failed querying user cars: %w", err)
 	}
-	// Query the inverse edge.
+
+	// query the inverse edge.
 	for _, ca := range cars {
 		owner, err := ca.QueryOwner().Only(ctx)
 		if err != nil {
@@ -140,5 +154,126 @@ func QueryCarUsers(ctx context.Context, a8m *ent.User) error {
 		}
 		log.Printf("car %q owner: %q\n", ca.Model, owner.Name)
 	}
+	return nil
+}
+
+func CreateGraph(ctx context.Context, client *ent.Client) error {
+	// First, create the users.
+	a8m, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("Ariel").
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	neta, err := client.User.
+		Create().
+		SetAge(28).
+		SetName("Neta").
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	// Then, create the cars, and attach them to the users in the creation.
+	err = client.Car.
+		Create().
+		SetModel("Tesla").
+		SetRegisteredAt(time.Now()). // ignore the time in the graph.
+		SetOwner(a8m).               // attach this graph to Ariel.
+		Exec(ctx)
+	if err != nil {
+		return err
+	}
+	err = client.Car.
+		Create().
+		SetModel("Mazda").
+		SetRegisteredAt(time.Now()). // ignore the time in the graph.
+		SetOwner(a8m).               // attach this graph to Ariel.
+		Exec(ctx)
+	if err != nil {
+		return err
+	}
+	err = client.Car.
+		Create().
+		SetModel("Ford").
+		SetRegisteredAt(time.Now()). // ignore the time in the graph.
+		SetOwner(neta).              // attach this graph to Neta.
+		Exec(ctx)
+	if err != nil {
+		return err
+	}
+	// Create the groups, and add their users in the creation.
+	err = client.Group.
+		Create().
+		SetName("GitLab").
+		AddUsers(neta, a8m).
+		Exec(ctx)
+	if err != nil {
+		return err
+	}
+	err = client.Group.
+		Create().
+		SetName("GitHub").
+		AddUsers(a8m).
+		Exec(ctx)
+	if err != nil {
+		return err
+	}
+	log.Println("The graph was created successfully")
+	return nil
+}
+
+func QueryGithub(ctx context.Context, client *ent.Client) error {
+	cars, err := client.Group.
+		Query().
+		Where(group.Name("GitHub")). // (Group(Name=GitHub),)
+		QueryUsers().                // (User(Name=Ariel, Age=30),)
+		QueryCars().                 // (Car(Model=Tesla, RegisteredAt=<Time>), Car(Model=Mazda, RegisteredAt=<Time>),)
+		All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed getting cars: %w", err)
+	}
+	log.Println("cars returned:", cars)
+	// Output: (Car(Model=Tesla, RegisteredAt=<Time>), Car(Model=Mazda, RegisteredAt=<Time>),)
+	return nil
+}
+
+func QueryArielCars(ctx context.Context, client *ent.Client) error {
+	// Get "Ariel" from previous steps.
+	a8m := client.User.
+		Query().
+		Where(
+			user.HasCars(),
+			user.Name("Ariel"),
+		).
+		OnlyX(ctx)
+	cars, err := a8m. // Get the groups, that a8m is connected to:
+				QueryGroups(). // (Group(Name=GitHub), Group(Name=GitLab),)
+				QueryUsers().  // (User(Name=Ariel, Age=30), User(Name=Neta, Age=28),)
+				QueryCars().   //
+				Where(         //
+			car.Not( //	Get Neta and Ariel cars, but filter out
+				car.ModelEQ("Mazda"), //	those who named "Mazda"
+			), //
+		). //
+		All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed getting cars: %w", err)
+	}
+	log.Println("cars returned:", cars)
+	return nil
+}
+
+func QueryGroupWithUsers(ctx context.Context, client *ent.Client) error {
+	groups, err := client.Group.
+		Query().
+		Where(group.HasUsers()).
+		All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed getting groups: %w", err)
+	}
+	log.Println("groups returned:", groups)
+	// Output: (Group(Name=GitHub), Group(Name=GitLab),)
 	return nil
 }
